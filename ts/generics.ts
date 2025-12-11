a001f902b621e73323ad5bfd02079a2e121a5ee5
SachinM44 <sachin69778@gmail.com>
2025-12-11 20:41:14 +0530

    learn generics deeply

--- ts/generics.ts
+++ ts/generics.ts
@@ -1,47 +1,48 @@
-// import { it } from "zod/locales"
+import { number } from "zod"
+import { it } from "zod/locales"
 
-// function genirc(args: number): number {
-// return args
-// }
-// ///// so here when in the above function is numebr , lets say u want to be it the global function and the u want it to work with the any type 
-// // what will u do 
+function genirc(args: number): number {
+return args
+}
+///// so here when in the above function is numebr , lets say u want to be it the global function and the u want it to work with the any type 
+// what will u do 
 
-// function  genericWithAny(args: any): any{
-// return args
-// } ///// so this  the wrong way of doign it 
-// //insted 
-// function generic<T>(arges:T[]): T {
-// return arges[0]
-// } 
+function  genericWithAny(args: any): any{
+return args
+} ///// so this  the wrong way of doign it 
+//insted 
+function generic<T>(arges:T[]): T {
+return arges[0]
+} 
 
-// // so u can use it at any time 
-// const ans=generic([
-//   {
-//     id:1,
-//     id2:3
-//   }
-// ])
+// so u can use it at any time 
+const ans=generic([
+  {
+    id:1,
+    id2:3
+  }
+])
 
-// function swaper(a:any,b:any):any{
-//  const temp=a   
-// a=b
-// b=temp
-// return [a, b]
-// }
-// console.log(ans.id)
+function swaper(a:any,b:any):any{
+ const temp=a   
+a=b
+b=temp
+return [a, b]
+}
+console.log(ans.id)
 
-// const swap=<T, U>(a:T, b:U):[T,U]=>{
-// return [a,b]
-// }
+const swap=<T, U>(a:T, b:U):[T,U]=>{
+return [a,b]
+}
 
-// const lol=swap(1,2)
-// console.log(lol)
+const lol=swap(1,2)
+console.log(lol)
 
 
-// console.log(swaper(1,3))
-////how will you restric generic
+console.log(swaper(1,3))
+//how will you restric generic
 
-/// one tip here im using this trialing comma hack where where u using extends keyword at this posing ts will complain if its ts file but the tsx file 
+// one tip here im using this trialing comma hack where where u using extends keyword at this posing ts will complain if its ts file but the tsx file 
 
 const ristGeneric = <T extends { length: number } ,>(item: T): T => {
    console.log(item.length)
@@ -50,3 +51,17 @@ const ristGeneric = <T extends { length: number } ,>(item: T): T => {
 
 console.log(ristGeneric({length:10}))
 console.log(ristGeneric([2,3,5]))
+
+
+///// for api example 
+interface IApiResponse<T>{
+    status:number,
+    data:T
+}
+
+const creatResouce=<T>(data:T):IApiResponse<T>=>{
+return {status : 200 , data}
+}
+console.log(creatResouce("Success"))
+console.log(creatResouce({ id: 1, name: "Sachin" }))
+console.log(creatResouce([1, 2, 3]))
