830c68f126776cf8df04eaeda2520618b208bf5e
SachinM44 <sachin69778@gmail.com>
2025-12-18 23:39:15 +0530

    solving mapLimits problem

--- javascript/LRB/MapLimit.js
+++ javascript/LRB/MapLimit.js
@@ -9,11 +9,35 @@ const number=[1,2,3,4,5]
 
 const limit =2;
 
-const mapLimit= (input, limit, iterable)=>{
-  return new Promise((res, rej)=>{
 
 
 
-  })
+/// first here start with chopping the array 
+const chopper=(size)=>{
+
+    const temp=[...this]
+    if(!size){
+        return temp
+    }
+    
+    const output=[];
+    let i=0;
+    while(i<temp.length){
+        output.push(temp.slice(i, i+size))
+        i=i+size;
+    }
+    return output
 }
-Array.prototype.chop
+         const mapLimit= (input, limit, fn)=>{
+          return new Promise((res, rej)=>{
+        //// then chop it
+        let chopped=input.chop(limit)
+             // for all the subarrays of chopped
+    // run it in series
+    // that is one after another
+    // initially it will take an empty array to resolve
+    // merge the output of the subarray and pass it on to the next
+    
+        
+          })
+        }
