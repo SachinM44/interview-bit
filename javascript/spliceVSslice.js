342d9f56246e81ac8967a67a81bee46a40bf6d57
SachinM44 <sachin69778@gmail.com>
2025-12-30 17:22:52 +0530

    feat(js) added object freez methods

--- javascript/spliceVSslice.js
+++ javascript/spliceVSslice.js
@@ -1,3 +1,5 @@
+const { object } = require("webidl-conversions");
+
 const array=[3,34,54,5,5,3,3,2,2,2]
 
 console.log(array.slice(0,4));
@@ -6,3 +8,30 @@ console.log(array.splice(1,4))
 console.log(array)
 
 ///so basically that splice thing will mutate ur origininal array entirely , and it can alter , delete , add etc ..
+class a {
+
+}
+console.log(typeof(a)
+)
+
+
+let arr=[3,4,5,6,2]
+arr.length=393
+console.log(arr)
+
+
+
+const user={
+    name:'bulla',
+    age:33,
+    location:'malpe',
+    wife:{
+        name:'pushpa',
+        age:33
+    }
+}
+
+Object.freeze(user)
+user.wife.age=344
+user.age=45
+console.log(user)
