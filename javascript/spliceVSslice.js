2babe909df1821408bf282499fc2b73b8b4c104e
SachinM44 <sachin69778@gmail.com>
2025-12-30 16:55:10 +0530

    feat(JS): added ./spliceVSslice.js

---
+++ javascript/spliceVSslice.js
@@ -0,0 +1,8 @@
+const array=[3,34,54,5,5,3,3,2,2,2]
+
+console.log(array.slice(0,4));
+console.log('-------------------------')
+console.log(array.splice(1,4))
+console.log(array)
+
+///so basically that splice thing will mutate ur origininal array entirely , and it can alter , delete , add etc ..
