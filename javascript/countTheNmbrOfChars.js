1b3e9a4c4035c971ad8e3791f54ebfaea40c81da
SachinM44 <sachin69778@gmail.com>
2026-01-03 00:39:56 +0530

    feat(js) added ./countTheNmbrOfChars.js

---
+++ javascript/countTheNmbrOfChars.js
@@ -0,0 +1,32 @@
+// let string='piperPiper';
+
+// let result=[];
+
+// for(let i=0; i <string.length; i++){
+//   let char=string[i];
+
+//   if(result[char]){
+//     result[char]++;
+//   }else{
+//     result[char]=1;
+//   }
+      
+// }
+
+// console.log(result);
+
+let string='babyPiperStop'
+
+let result=[];
+
+for(let i=0; i <string.length; i++){
+    let char=string[i]//b
+    if(result[char]){
+      result[char]++;
+    }else{
+      result[char] = 1; 
+    }
+}
+
+console.log(result)
+
