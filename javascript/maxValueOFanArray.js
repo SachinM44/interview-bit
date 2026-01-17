7801d70b2d6c92cc1b55f496cd2a18b5973a961e
SachinM44 <sachin69778@gmail.com>
2026-01-17 23:54:26 +0530

    feat: added max value of an array

---
+++ javascript/maxValueOFanArray.js
@@ -0,0 +1,37 @@
+////in the given array , you have to get any two number where you can gain the max 2 numbers
+
+// const arr = [1, 2, 3, 9, 4, 5, 6];
+
+// const maxSum = (arr) => {
+//   const sorted = arr.sort((a, b) => b - a);
+//   return arr[0] * arr[1]
+// };
+
+
+// console.log(maxSum(arr))
+
+//  const abj1={
+//     title1:'raju',
+//     age3:10
+//  }
+
+
+//   const obj2={
+//     title2:'rajuRam',
+//     age4:30
+//  }
+//  const abj3={
+//     ...abj1,
+//     ...obj2
+//  }
+
+//  console.log(abj3)
+
+var x=10;
+{
+    var x=3;
+}
+let a=x;
+{
+    
+}
