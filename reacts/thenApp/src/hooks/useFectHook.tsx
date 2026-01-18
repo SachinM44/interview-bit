7ae240f1572f8b6199c0a36eb0be54205ab1752e
SachinM44 <sachin69778@gmail.com>
2026-01-18 23:07:06 +0530

    feat: added somthing

---
+++ reacts/thenApp/src/hooks/useFectHook.tsx
@@ -0,0 +1,22 @@
+import { asyncWrapProviders } from "async_hooks";
+import { useEffect, useState, useTransition } from "react"
+
+
+export const useFetch = (url: string) => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+    const [data, setDate] = useState();
+
+    useEffect(() => {
+
+        const fetchAPi = async () => {
+            const data = await fetch(url);
+            const respose = await data.json()
+            setDate(respose)
+            setLoading(false)
+
+        }
+
+        fetchAPi()
+    }, [url])
+}
