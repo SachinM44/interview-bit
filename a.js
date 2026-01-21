e7dda0f407dcfa4b8e8a15a5b11cf6881f5d4778
SachinM44 <sachin69778@gmail.com>
2026-01-21 13:19:45 +0530

    fnfn

--- a.js
+++ a.js
@@ -1,15 +1,15 @@
-
-
-
-const outFN=()=>{
-    let counter=0
-    return ()=>{
-        counter=counter+3;
-        return counter
+//let me write once again
+const countTheName = (string) => {
+  let result = [];
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (result[char]) {
+      result[char]++;
+    } else {
+      result[char] = 1;
     }
-}
-
-const result=outFN();
-console.log(result())
-console.log(result())
-console.log(result())
+  }
+  return result;
+};
+let string='dhdhhhhdhhd'
+console.log(countTheName(string))
