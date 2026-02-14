a1be74cd2f44cf6a63deb62a5fa17397b524b398
SachinM44 <sachin69778@gmail.com>
2026-02-14 15:48:27 +0530

    feat: added map in js

--- reacts/counter-app/src/componets/button.tsx
+++ reacts/counter-app/src/componets/button.tsx
@@ -5,11 +5,15 @@ interface ButtonProps {
     onClick: () => void
 }
 
+const data=['banana', 'mango', 'cherry']
+
 const Button = ({ name, onClick }: ButtonProps) => {
     return (
         <div className="flex flex-row">
             <button onClick={onClick}>{name}</button>
-
+              {data.map((item,index)=>(
+           <Text key={index}>{item}</Text>
+              ))}
         </div>
     )
 }
