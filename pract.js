c02165e1c9ec4fa3171a4ebe3e14b72a9aaef79b
SachinM44 <sachin69778@gmail.com>
2026-02-22 12:53:16 +0530

    feat: added somthing: pract

--- pract.js
+++ pract.js
@@ -1,19 +1,43 @@
-///debound
+// ///debound
 
-const debounce = (fn, delay) => {
-  let timer;
+// const debounce = (fn, delay) => {
+//   let timer;
+//   return (...args) => {
+//     clearInterval(timer);
+//     timer = setTimeout(() => {
+//       fn.apply(this, args);
+//     }, delay);
+//   };
+// };
+
+// /// to use that
+
+// const debouncedCall = debounce(() => {
+//   console.log("hello from debounce");
+// }, 5000);
+
+// debouncedCall();
+
+///// throttling
+
+const throttling = (fn, delay) => {
+  let lastCall = 0;
   return (...args) => {
-    clearInterval(timer);
-    timer = setTimeout(() => {
+    const now = Date.now();
+    console.log(now);
+    if (now - lastCall >= delay) {
+      lastCall = now;
       fn.apply(this, args);
-    }, delay);
+    }
   };
 };
 
-/// to use that
+const thorttledFN = throttling(() => {
+  console.log("hello there from thottling");
+}, 20000);
 
-const debouncedCall = debounce(() => {
-  console.log("hello from debounce");
-}, 5000);
+//thorttledFN();
 
-debouncedCall();
+setInterval(() => {
+    thorttledFN()
+}, 500);
