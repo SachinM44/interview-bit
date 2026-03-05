f820a9440d8de0bc916a3b20ac6682627de50f07
SachinM44 <sachin69778@gmail.com>
2026-03-05 21:35:30 +0530

    feat: keeping it in a flow

--- new/scrit.js
+++ new/scrit.js
@@ -1,20 +1,20 @@
-// // // const x=20;
-// // // function foo(){
-// // //       var x=10;
-// // //     }
-// // //     foo();
-// // //     console.log( x)
-// // foo();
-// // var foo=10;
-// // function foo(){
-// //     console.log("hello from foo ")
-// // }
-// // foo();
-// for(let i=1; i<=3;i++){
-//     setTimeout(()=>{
-//         console.log(i)
-//     },1000)
-// }
+const x=20;
+function foo(){
+      var x=10;
+    }
+    foo();
+    console.log( x)
+foo();
+var foo=10;
+function foo(){
+    console.log("hello from foo ")
+}
+foo();
+for(let i=1; i<=3;i++){
+    setTimeout(()=>{
+        console.log(i)
+    },1000)
+}
 //......................
 // setTimeout(() => {
 //    console.log("setTimeOut") 
