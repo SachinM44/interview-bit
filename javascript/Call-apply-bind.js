e6cbaba5b554c97e0dacf472f81a161f4de981f1
SachinM44 <sachin69778@gmail.com>
2026-04-30 08:14:26 +0530

    feat: added somthing new

--- javascript/Call-apply-bind.js
+++ javascript/Call-apply-bind.js
@@ -1,27 +1,26 @@
-// waiter 
+// waiter
 
-function cook(ing, ing2, in3){
-    console.log(`${this.name} is having the ${ing} ${ing2} ${in3}`)
+function cook(ing, ing2, in3) {
+  console.log(`${this.name} is having the ${ing} ${ing2} ${in3}`);
 }
-const adam={name: "adam"}
-cook.call(adam, 'rice', 'plat', 'chiken')
-// what the thing here is that by using the call ican pass the argument to the function 
-// basically it call the parameters one by one 
+const adam = { name: "adam" };
+cook.call(adam, "rice", "plat", "chiken");
+// what the thing here is that by using the call ican pass the argument to the function
+// basically it call the parameters one by one
 //////////////////////////////////////
-// / in the applay it set to array but not the list as mentioned above 
-cook.call(adam, ['rice', 'plat', 'chiken'])
+// / in the applay it set to array but not the list as mentioned above
+cook.call(adam, ["rice", "plat", "chiken"]);
 ////////////
 
 ///
-/// call - runs the function immidiatly ,  passing each argument separetly 
-// applay-where the apply do that same but it pass the argument in an array format 
-// bind- creates a new function with present this and argument which u can call later 
+/// call - runs the function immidiatly ,  passing each argument separetly
+// applay-where the apply do that same but it pass the argument in an array format
+// bind- creates a new function with present this and argument which u can call later
 
+const fn = (a1, a2, a3) => {
+  console.log(`${this.age} hello ${a1}, ${a2}`);
+};
 
-const fn=(a1,a2, a3)=>{
-console.log(`${this.age} hello ${a1}, ${a2}`)
-}
+const man = { age: 20 };
 
-const man={age: 20}
-
-fn.call(adam, 33, 33)
+fn.call(adam, 33, 33);
