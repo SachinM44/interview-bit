850a480ca64efbbe76d3c24a34282106e2ba72ed
SachinM44 <sachin69778@gmail.com>
2026-09-12 23:30:19 +0530

    feat(llm) : added time trackign by the llm

--- python/llm/llm_call.py
+++ python/llm/llm_call.py
@@ -7,10 +7,15 @@ from google import genai
 
 client = genai.Client()
 
+start_time = time.perf_counter() # so this is the fn which we use for the performace counting likle that 
 interaction = client.interactions.create(
     model="gemini-3.5-flash",
     input="Explain how AI works in a few words"
 )
 
+end_time= time.perf_counter()
 
-print(interaction.output.text)
+
+print(interaction.output_text)
+
+print(f"time taken by api gemini is {end_time-start_time} seconds")
