c22e56d5869a3c2c7ab9da56ac5b61ab8dc22c7b
SachinM44 <sachin69778@gmail.com>
2026-09-19 23:59:55 +0530

    feat: added some random ai work

--- python/llm/llm_call.py
+++ python/llm/llm_call.py
@@ -7,15 +7,30 @@ from google import genai
 
 client = genai.Client()
 
-start_time = time.perf_counter() # so this is the fn which we use for the performace counting likle that 
-interaction = client.interactions.create(
-    model="gemini-3.5-flash",
-    input="Explain how AI works in a few words"
-)
+# #start_time = time.perf_counter() # so this is the fn which we use for the performace counting likle that 
+# # interaction = client.interactions.create(
+# #     model="gemini-3.5-flash",
+# #     input="Explain how AI works in a few words"
+# # )
 
-end_time= time.perf_counter()
+# end_time= time.perf_counter()
 
 
-print(interaction.output_text)
+def cook_food(ingredient, cusine, diet):
+    prompt= f'''
+      first sing me a song in kannada short and 2 lines in english and 2 lines in kannada 
+     '''
+    interaction = client.interactions.create(
+         model="gemini-3.5-flash",
+         input=prompt 
+    )
+    print(interaction.output_text)
+    return interaction.output_text
 
-print(f"time taken by api gemini is {end_time-start_time} seconds")
+
+
+
+cook_food(["tomato" , "onion"], "indian", "veg")
+
+
+# print(f"time taken by api gemini is {end_time-start_time} seconds")
